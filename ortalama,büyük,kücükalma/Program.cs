eacaed145dfc7b5270af5101402981c78a76b0f9
Mehmet Yıldız <mhmet71yildiz@gmail.com>
2023-10-25 11:42:06 +0300

    Charpbasic

---
+++ ortalama,büyük,kücükalma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class Program{
+
+        static void Main(string[] args){
+            Console.WriteLine("3 adet sayi giriniz");
+            Console.Write("1. sayiyi giriniz: ");
+            int x= Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. sayiyi giriniz: ");
+            int y= Convert.ToInt32(Console.ReadLine());
+            Console.Write("3.sayiyi girirniz: ");
+            int z= Convert.ToInt32(Console.ReadLine());
+
+            int[] xyz = new int[3]{x,y,z};
+            int ort= ((x+y+z)/3);
+
+            Console.WriteLine("En küçük sayi: " + xyz.Min()); //.Min komutu ile girilen sayılardan en küçüğünü istedik
+            Console.WriteLine("En büyük sayi: " + xyz.Max()); //.Max komutu ile girilen sayılardan en büyüğünü istedik
+            Console.WriteLine("girdiğiniz sayilarin ortalamasi: " + ort);
+
+        }
+    }
---
+++ yazilardankelimeyapma/yazılanrakamıkelimeyapma.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace rakamdankelime{
+
+    class Program{
+
+        static void Main(string[]args){
+
+            basasar: //konum etiketi
+            Console.WriteLine("bir rakam giriniz");
+            int rakam = Convert.ToInt32(Console.ReadLine());
+
+            switch(rakam){
+                case 0:
+                    Console.WriteLine("yazdığınız rakam 'sıfır'");
+                    break;
+                case 1:
+                    Console.WriteLine("yazdığınız rakam 'bir'");
+                    break;
+                case 2:
+                    Console.WriteLine("yazdığınız rakam 'iki'");
+                    break;
+                case 3:
+                    Console.WriteLine("yazdığınız rakam 'üç'");
+                    break;
+                case 4:
+                    Console.WriteLine("yazdığınız rakam 'dört'");
+                    break;
+                case 5:
+                    Console.WriteLine("yazdığınız rakam 'beş'");
+                    break;
+                case 6:
+                    Console.WriteLine("yazdığınız rakam 'altı'");
+                    break;
+                case 7:
+                    Console.WriteLine("yazdığınız rakam 'yedi'");
+                    break;
+                case 8:
+                    Console.WriteLine("yazdığınız rakam 'sekiz'");
+                    break;
+                case 9:
+                    Console.WriteLine("yazdığınız rakam 'dokuz'");
+                    break;
+                default:
+                    Console.WriteLine("yanlış bir sayı girdiniz lütfen rakam giriniz:");
+                    goto basasar; //hatalı girilme durumunda programın tekrar çalışması için
+                    break; 
+                
+            }       
+        }
+    }
+}
