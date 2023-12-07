4747fe98b6563da39cc9c0ac916c283f4f78f4b2
Mehmet Yıldız <mhmet71yildiz@gmail.com>
2023-12-07 16:47:23 +0300

    ürün indirimi

---
+++ ürünindirimi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string[] args){
+
+        const double indirim1 = 0.10;
+        const double indirim2 = 0.20;
+        const double alimsiniri = 5000.0;
+
+        Console.WriteLine("alinan ürün sayisini giriniz: ");
+        int ürünsayisi = int.Parse(Console.ReadLine());
+
+        double toplamürünmiktari=0.0;
+        
+        for(int i=1;i<=ürünsayisi;i++){
+            Console.WriteLine($"Lütfen {i}. ürünün fiyatini giriniz: ");
+            double ürünfiyati = double.Parse(Console.ReadLine());
+
+            toplamürünmiktari += ürünfiyati;
+        }
+
+        double indirimtutari = 0.0;
+
+        if(toplamürünmiktari<alimsiniri){
+            
+            indirimtutari = toplamürünmiktari * indirim1;
+        }
+        else{
+            indirimtutari = toplamürünmiktari * indirim2;
+        }
+        double ödememiktari = toplamürünmiktari - indirimtutari;
+
+        Console.WriteLine($"alişveriş tutari : {toplamürünmiktari}");
+        Console.WriteLine($"indirim miktari : {indirimtutari}");
+        Console.WriteLine($"ödenecek tutar: {ödememiktari}");
+    }
+}
