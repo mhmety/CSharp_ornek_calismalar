69e740cd8663391e33d7766d87eaf8f242191505
Mehmet Yıldız <mhmet71yildiz@gmail.com>
2023-12-09 15:36:35 +0300

    CSharp

---
+++ dunyanufusartisi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const double growthRate = 0.011; 
+        double currentPopulation = 7.9e9; 
+
+        Console.WriteLine("Yıl\tBeklenen Nüfus\tArtış\t\tArtış Miktarı");
+
+        for (int year = 1; year <= 75; year++)
+        {
+            double expectedPopulation = currentPopulation * (1 + growthRate);
+            double populationIncrease = expectedPopulation - currentPopulation;
+
+            Console.WriteLine($"{year}\t{expectedPopulation:F0}\t\t{populationIncrease:F0}\t\t{growthRate * 100:F2}%");
+
+            currentPopulation = expectedPopulation;
+        }
+
+        Console.WriteLine("\nNüfusun iki katına çıkacağı yıl: " + FindDoublingYear(currentPopulation, growthRate));
+    }
+
+    static int FindDoublingYear(double currentPopulation, double growthRate)
+    {
+        int year = 0;
+
+        while (currentPopulation < currentPopulation * 2)
+        {
+            currentPopulation = currentPopulation * (1 + growthRate);
+            year++;
+        }
+
+        return year;
+    }
+}
---
+++ faktoriyel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Doğal sayı girin: ");
+        int n;
+
+        while (!int.TryParse(Console.ReadLine(), out n) || n < 0)
+        {
+            Console.Write("Geçerli bir doğal sayı girin: ");
+        }
+
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"{n}! = {factorial}");
+    }
+}
