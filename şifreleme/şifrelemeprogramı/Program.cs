56c5fa9dc560afcdc17feaa55bcc5035bf1e2651
Mehmet Yıldız <mhmet71yildiz@gmail.com>
2023-12-09 15:56:01 +0300

    password

---
+++ şifreleme/şifrelemeprogramı/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Dört basamaklı bir tam sayı girin: ");
+        int originalNumber;
+
+        while (!int.TryParse(Console.ReadLine(), out originalNumber) || originalNumber < 1000 || originalNumber > 9999)
+        {
+            Console.Write("Geçerli bir dört basamaklı tam sayı girin: ");
+        }
+
+        int encryptedNumber = Encrypt(originalNumber);
+        Console.WriteLine($"Şifrelenmiş sayı: {encryptedNumber:D4}");
+    }
+
+    static int Encrypt(int number)
+    {
+        int digit1 = (number + 7) % 10;
+        int digit2 = (number / 1000 + 7) % 10;
+        int digit3 = (number / 100 % 10 + 7) % 10;
+        int digit4 = (number / 10 % 10 + 7) % 10;
+
+        return digit2 * 1000 + digit1 * 100 + digit4 * 10 + digit3;
+    }
+}
---
+++ şifreleme/şifreçözümprogramı/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Şifrelenmiş dört basamaklı bir tam sayı girin: ");
+        int encryptedNumber;
+
+        while (!int.TryParse(Console.ReadLine(), out encryptedNumber) || encryptedNumber < 1000 || encryptedNumber > 9999)
+        {
+            Console.Write("Geçerli bir dört basamaklı şifrelenmiş tam sayı girin: ");
+        }
+
+        int decryptedNumber = Decrypt(encryptedNumber);
+        Console.WriteLine($"Çözülen sayı: {decryptedNumber:D4}");
+    }
+
+    static int Decrypt(int number)
+    {
+        int digit1 = (number % 100) / 10;
+        int digit2 = (number % 10);
+        int digit3 = (number / 1000);
+        int digit4 = (number / 100) % 10;
+
+        int originalNumber = (digit3 * 1000) + (digit1 * 100) + (digit4 * 10) + digit2;
+        originalNumber -= 7;
+
+        if (originalNumber < 1000)
+        {
+            originalNumber += 10000;
+        }
+
+        return originalNumber;
+    }
+}
