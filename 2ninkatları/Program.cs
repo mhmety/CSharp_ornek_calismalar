57fb7ce8f534c5ab162cd216a2c996b527439dda
Mehmet Yıldız <mhmet71yildiz@gmail.com>
2023-12-09 14:40:39 +0300

    CSharp exercises

---
+++ 2ninkatları/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program{
+    static void Main(){
+        double ikininkatlari = 1;
+
+        for(int i=0 ;i<40;i++){
+            Console.WriteLine(ikininkatlari);
+            ikininkatlari *= 2;
+            
+        }
+    }
+}
